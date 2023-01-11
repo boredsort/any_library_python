f269479e492a9ea59649098d8ec948a856c5fc6b
Kenn Arthur Variacion <kenn.variacion@inriver.com>
2023-01-11 16:14:46 +0800

    4. Interface segregation: Added separate interface for auth_sms

--- solid_principles/solid.py
+++ solid_principles/solid.py
@@ -23,12 +23,25 @@ class PaymentProcessor(ABC):
     def pay(self, order):
         pass
 
-class DebitPaymentProcessor(PaymentProcessor):
+class PaymentProcessor_SMS(PaymentProcessor):
+
+    @abstractmethod
+    def auth_sms(self, code):
+        pass
+
+class DebitPaymentProcessor(PaymentProcessor_SMS):
 
     def __init__(self, security_code):
         self.security_code = security_code
+        self.verified = False
+
+    def auth_sms(self, code):
+        print(f"Verifying SMS code {code}")
+        self.verified = True
 
     def pay(self, order):
+        if not self.verified:
+            raise Exception("Not Authorized")
         print("Processing debit payment type") 
         print(f"Verifying security code: {self.security_code}")
         order.status = "paid"
@@ -43,11 +56,19 @@ class CreditPaymentProcessor(PaymentProcessor):
         print(f"Verifying security code: {self.security_code}")
         order.status = "paid"
 
-class PaypalPaymentProcessor(PaymentProcessor):
+class PaypalPaymentProcessor(PaymentProcessor_SMS):
 
     def __init__(self, email_add):
         self.email_add = email_add
+        self.verified = False
+
+    def auth_sms(self, code):
+        print(f"Verifying SMS code {code}")
+        self.verified = True
+
     def pay(self, order):
+        if not self.verified:
+            raise Exception("Not Authorized")
         print("Processing paypal payment type")
         print(f"Verifying email_add: {self.email_add}")
         order.status = "paid"
@@ -59,10 +80,15 @@ order.add_item("USB cable", 2, 5)
 
 print(order.total_price())
 
+payment_processor = CreditPaymentProcessor("7011111")
+payment_processor.pay(order)
+
 payment_processor = DebitPaymentProcessor("7011111")
+payment_processor.auth_sms('40044')
 payment_processor.pay(order)
 
 payment_processor = PaypalPaymentProcessor("batman@gmail.com")
+payment_processor.auth_sms('40044')
 payment_processor.pay(order)
 
 
